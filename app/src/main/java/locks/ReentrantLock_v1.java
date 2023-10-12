39d46aad39d777877fbd88deca5df983449dd80d
Raphael Fontoura <raphael.fontoura@gmail.com>
2023-10-12 16:45:50 -0300

    Add reentranceLock tests

---
+++ app/src/main/java/locks/ReentrantLock_v1.java
@@ -0,0 +1,34 @@
+package locks;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+/**
+ * ReentrantLock_v1
+ */
+public class ReentrantLock_v1 {
+
+  private static int i = -1;
+  private static Lock lock = new ReentrantLock();
+
+  public static void main(String[] args) {
+    ExecutorService executor = Executors.newCachedThreadPool();
+
+    Runnable r1 = () -> {
+      lock.lock();
+      String name = Thread.currentThread().getName();
+      i++;
+      System.out.println(name + " : " + i);
+      lock.unlock();
+    };
+
+    for (int i = 0; i < 6; i++) {
+      executor.execute(r1);
+    }
+
+    executor.shutdown();
+
+  }
+}
---
+++ app/src/main/java/locks/ReentrantReadWriteLock_v1.java
@@ -0,0 +1,41 @@
+package locks;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReadWriteLock;
+import java.util.concurrent.locks.ReentrantLock;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
+
+public class ReentrantReadWriteLock_v1 {
+  private static int i = -1;
+  private static ReadWriteLock lock = new ReentrantReadWriteLock();
+
+  public static void main(String[] args) {
+    ExecutorService executor = Executors.newCachedThreadPool();
+
+    Runnable r1 = () -> {
+      lock.writeLock().lock();
+      String name = Thread.currentThread().getName();
+      System.out.println("ESCREVENDO: " + name + " : " + i++);
+      System.out.println("ESCRITO: " + name + " : " + i);
+      lock.writeLock().unlock();
+    };
+
+    Runnable r2 = () -> {
+      lock.readLock().lock();
+      String name = Thread.currentThread().getName();
+      System.out.println("LENDO: " + name + " : " + i);
+      System.out.println("LIDO: " + name + " : " + i);
+      lock.readLock().unlock();
+    };
+
+    for (int i = 0; i < 6; i++) {
+      executor.execute(r1);
+      executor.execute(r2);
+    }
+
+    executor.shutdown();
+
+  }
+}
